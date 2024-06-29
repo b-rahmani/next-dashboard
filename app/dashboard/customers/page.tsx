f83c916b53616bb14bc02beaf6f53158124b4762
behnam <behnam.rahmani8@gmail.com>
2024-06-29 18:27:51 +0330

    initial dashboard

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,6 @@
+const Page = () => {
+    return (
+        <div>customers page</div>
+    )
+}
+export default Page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+const Page = () => {
+    return (
+        <div className="bg-red-500">invoices Page</div>
+    )
+}
+export default Page
